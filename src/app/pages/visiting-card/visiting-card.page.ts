8c67679fdfd0b7a5ee8f9717330bf019361a01d8
Kinjal Modi <kvmodi19@gmail.com>
2020-10-17 12:46:09 +0530

    changes to send notification for shop visited

--- src/app/pages/visiting-card/visiting-card.page.ts
+++ src/app/pages/visiting-card/visiting-card.page.ts
@@ -9,6 +9,8 @@ import { Professions } from '../../models/users.model';
 import { FeedsApiService } from '../../services/api/feeds.api.service';
 import { ServiceProvider } from 'src/app/models/service-provider.model';
 import { ActionService } from 'src/app/services/component/action.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { NotificationApiService } from 'src/app/services/api/notification.api.service';
 
 @Component({
 	selector: 'app-visiting-card',
@@ -22,6 +24,7 @@ export class VisitingCardPage {
 	selectedSegment = 'about';
 	serviceDetails: ServiceProvider;
 	professions = Professions;
+	userService: boolean;
 
 	constructor(
 		private navCtrl: NavController,
@@ -29,13 +32,25 @@ export class VisitingCardPage {
 		private feedService: FeedsApiService,
 		private actionService: ActionService,
 		private socialSharing: SocialSharing,
+		private authService: AuthenticationService,
+		private notificationService: NotificationApiService
 	) { }
 
 	ionViewWillEnter() {
 		this.route.params.subscribe((params: { id: string }) => {
+			const user = this.authService.getUser();
+			this.userService = user.shopOwner && user.serviceId === params.id;
 			this.feedService.getById(params.id)
 				.then((response) => {
 					this.serviceDetails = response.serviceProvider;
+					if (!this.userService) {
+						const notification = {
+							title: 'shop visited',
+							description: `${user.name} viited the shop`,
+							user: response.serviceProvider.user._id
+						} as any;
+						this.notificationService.addNotification(notification);
+					}
 				})
 				.catch((error) => {
 					console.log(error);
